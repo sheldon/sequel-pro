sheldon/sequel-pro

commit 8a0080b6fea47a38dba89ecbe0e3b364490737cf

- Convert connection encoding menus to be menu tag based to fix localisation errors  - Add database encoding retrieval support for MySQL 4.1  - Convert the add table and add database sheets to use encoding menus derived from server supported encodings  - Re-layout preferences with larger labels to aid localisation  - Fix preference resizing in non-Favorite tabs
git-svn-id: http://sequel-pro.googlecode.com/svn/trunk@2325 eefc5464-0a4a-0410-82e6-d75a05d1654a

Source/SPTablesList.m
@@ -287,6 +287,35 @@
 		[tableTypeButton addItemWithTitle:[engine objectForKey:@"Engine"]];
 	}
 	
+	// Populate the table encoding popup button with a default menu item
+	[tableEncodingButton removeAllItems];
+	[tableEncodingButton addItemWithTitle:@"Default"];
+
+	// Retrieve the server-supported encodings and add them to the menu
+	NSArray *encodings  = [databaseDataInstance getDatabaseCharacterSetEncodings];
+	NSString *utf8MenuItemTitle = nil;
+	if ([encodings count] > 0
+		&& ([mySQLConnection serverMajorVersion] > 4
+			|| ([mySQLConnection serverMajorVersion] == 4 && [mySQLConnection serverMinorVersion] >= 1)))
+	{
+		[[tableEncodingButton menu] addItem:[NSMenuItem separatorItem]];
+		for (NSDictionary *encoding in encodings) {
+			NSString *menuItemTitle = (![encoding objectForKey:@"DESCRIPTION"]) ? [encoding objectForKey:@"CHARACTER_SET_NAME"] : [NSString stringWithFormat:@"%@ (%@)", [encoding objectForKey:@"DESCRIPTION"], [encoding objectForKey:@"CHARACTER_SET_NAME"]];
+			[tableEncodingButton addItemWithTitle:menuItemTitle];
+
+			// If the UTF8 entry has been encountered, store the menu title
+			if ([[encoding objectForKey:@"CHARACTER_SET_NAME"] isEqualToString:@"utf8"]) {
+				utf8MenuItemTitle = [NSString stringWithString:menuItemTitle];
+			}
+		}
+
+		// If a UTF8 entry was found, promote it to the top of the list
+		if (utf8MenuItemTitle) {
+			[[tableEncodingButton menu] insertItem:[NSMenuItem separatorItem] atIndex:2];
+			[tableEncodingButton insertItemWithTitle:utf8MenuItemTitle atIndex:2];
+		}
+	}
+	
 	[NSApp beginSheet:tableSheet
 	   modalForWindow:[tableDocumentInstance parentWindow]
 		modalDelegate:self
@@ -675,7 +704,7 @@
 
 		// If encoding is set to Autodetect, update the connection character set encoding
 		// based on the newly selected table's encoding - but only if it differs from the current encoding.
-		if ([[[NSUserDefaults standardUserDefaults] objectForKey:SPDefaultEncoding] isEqualToString:@"Autodetect"]) {
+		if ([[[NSUserDefaults standardUserDefaults] objectForKey:SPDefaultEncoding] intValue] == SPEncodingAutodetect) {
 			if (tableEncoding != nil && ![tableEncoding isEqualToString:[tableDocumentInstance connectionEncoding]]) {
 				[tableDocumentInstance setConnectionEncoding:tableEncoding reloadingViews:NO];
 				[tableDataInstance resetAllData];
@@ -2033,7 +2062,7 @@
 		
 	// If there is an encoding selected other than the default we must specify it in CREATE TABLE statement
 	if ([tableEncodingButton indexOfSelectedItem] > 0) {
-		charSetStatement = [NSString stringWithFormat:@"DEFAULT CHARACTER SET %@", [[tableDocumentInstance mysqlEncodingFromDisplayEncoding:[tableEncodingButton title]] backtickQuotedString]];
+		charSetStatement = [NSString stringWithFormat:@"DEFAULT CHARACTER SET %@", [[tableDocumentInstance mysqlEncodingFromEncodingTag:[tableEncodingButton tag]] backtickQuotedString]];
 	}
 	
 	// If there is a type selected other than the default we must specify it in CREATE TABLE statement

Interfaces/English.lproj/MainMenu.xib
@@ -2,7 +2,7 @@
 <archive type="com.apple.InterfaceBuilder3.Cocoa.XIB" version="7.10">
 	<data>
 		<int key="IBDocument.SystemTarget">1050</int>
-		<string key="IBDocument.SystemVersion">10D573</string>
+		<string key="IBDocument.SystemVersion">10D2094</string>
 		<string key="IBDocument.InterfaceBuilderVersion">762</string>
 		<string key="IBDocument.AppKitVersion">1038.29</string>
 		<string key="IBDocument.HIToolboxVersion">460.00</string>
@@ -13,6 +13,7 @@
 		<object class="NSMutableArray" key="IBDocument.EditedObjectIDs">
 			<bool key="EncodedWithXMLCoder">YES</bool>
 			<integer value="956"/>
+			<integer value="29"/>
 		</object>
 		<object class="NSArray" key="IBDocument.PluginDependencies">
 			<bool key="EncodedWithXMLCoder">YES</bool>
@@ -1250,6 +1251,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">10</int>
 											</object>
 											<object class="NSMenuItem" id="428425210">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1260,6 +1262,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">20</int>
 											</object>
 											<object class="NSMenuItem" id="345290219">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1268,6 +1271,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">30</int>
 											</object>
 											<object class="NSMenuItem" id="649847059">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1279,6 +1283,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">-1</int>
 											</object>
 											<object class="NSMenuItem" id="587809847">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1289,6 +1294,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">40</int>
 											</object>
 											<object class="NSMenuItem" id="247892805">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1299,6 +1305,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">50</int>
 											</object>
 											<object class="NSMenuItem" id="1045933524">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1309,6 +1316,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">60</int>
 											</object>
 											<object class="NSMenuItem" id="929144169">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1320,6 +1328,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">-1</int>
 											</object>
 											<object class="NSMenuItem" id="1019891968">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1330,6 +1339,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">70</int>
 											</object>
 											<object class="NSMenuItem" id="272867556">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1340,6 +1350,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">80</int>
 											</object>
 											<object class="NSMenuItem" id="831843460">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1351,6 +1362,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">-1</int>
 											</object>
 											<object class="NSMenuItem" id="114701997">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1361,6 +1373,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">90</int>
 											</object>
 											<object class="NSMenuItem" id="86485907">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1371,6 +1384,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">100</int>
 											</object>
 											<object class="NSMenuItem" id="243329592">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1381,6 +1395,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">110</int>
 											</object>
 											<object class="NSMenuItem" id="620428396">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1391,6 +1406,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">120</int>
 											</object>
 											<object class="NSMenuItem" id="145813245">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1402,6 +1418,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">-1</int>
 											</object>
 											<object class="NSMenuItem" id="290468717">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1412,6 +1429,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">130</int>
 											</object>
 											<object class="NSMenuItem" id="384948978">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1423,6 +1441,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">-1</int>
 											</object>
 											<object class="NSMenuItem" id="537895346">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1433,6 +1452,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">140</int>
 											</object>
 											<object class="NSMenuItem" id="239164973">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1444,6 +1464,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">-1</int>
 											</object>
 											<object class="NSMenuItem" id="803749958">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1454,6 +1475,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">150</int>
 											</object>
 											<object class="NSMenuItem" id="691508085">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1464,6 +1486,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">160</int>
 											</object>
 											<object class="NSMenuItem" id="705318648">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1474,6 +1497,7 @@
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
 												<string key="NSAction">_popUpItemAction:</string>
+												<int key="NSTag">170</int>
 											</object>
 											<object class="NSMenuItem" id="302542110">
 												<reference key="NSMenu" ref="487618540"/>
@@ -1482,6 +1506,7 @@
 												<int key="NSMnemonicLoc">2147483647</int>
 												<reference key="NSOnImage" ref="625762401"/>
 												<reference key="NSMixedImage" ref="315854375"/>
+												<int key="NSTag">180</int>
 											</object>
 										</object>
 									</object>
@@ -4767,7 +4792,7 @@
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<integer value="1"/>
 					<string>{{449, 1007}, {197, 53}}</string>
-					<string>{{108, 563}, {511, 20}}</string>
+					<string>{{106, 685}, {511, 20}}</string>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<integer value="1"/>
 					<string>{{506, 836}, {511, 20}}</string>
@@ -4809,7 +4834,7 @@
 					<integer value="1"/>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<integer value="1"/>
-					<string>{{596, 277}, {255, 253}}</string>
+					<string>{{348, 432}, {255, 253}}</string>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<integer value="1"/>
 					<string>{{312, 683}, {231, 153}}</string>
@@ -4821,7 +4846,7 @@
 					<integer value="1"/>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<integer value="1"/>
-					<string>{{815, 44}, {276, 423}}</string>
+					<string>{{603, 82}, {276, 423}}</string>
 					<string>com.apple.InterfaceBuilder.CocoaPlugin</string>
 					<integer value="1"/>
 					<string>{{556, 185}, {279, 383}}</string>

Resources/PreferenceDefaults.plist
@@ -2,8 +2,8 @@
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>DefaultEncoding</key>
-	<string>Autodetect</string>
+	<key>DefaultEncodingTag</key>
+	<real>0</real>
 	<key>DefaultViewMode</key>
 	<integer>0</integer>
 	<key>LastViewMode</key>

Source/SPConstants.h
@@ -151,6 +151,30 @@ typedef enum
 	SPMainMenuHelp      = 7
 } SPMainMenuTags;
 
+// Encoding constants
+typedef enum
+{
+	SPEncodingAutodetect		= 0,
+	SPEncodingUCS2				= 10,
+	SPEncodingUTF8				= 20,
+	SPEncodingUTF8viaLatin1		= 30,
+	SPEncodingASCII				= 40,
+	SPEncodingLatin1			= 50,
+	SPEncodingMacRoman			= 60,
+	SPEncodingCP1250Latin2		= 70,
+	SPEncodingISOLatin2			= 80,
+	SPEncodingCP1256Arabic		= 90,
+	SPEncodingGreek				= 100,
+	SPEncodingHebrew			= 110,
+	SPEncodingLatin5Turkish		= 120,
+	SPEncodingCP1257WinBaltic	= 130,
+	SPEncodingCP1251WinCyrillic = 140,
+	SPEncodingBig5Chinese		= 150,
+	SPEncodingShiftJISJapanese	= 160,
+	SPEncodingEUCJPJapanese		= 170,
+	SPEncodingEUCKRKorean		= 180
+} SPEncodingTypes;
+
 // Long running notification time for Growl messages
 extern const CGFloat SPLongRunningNotificationTime;
 

Source/SPConstants.m
@@ -59,7 +59,7 @@ NSString *SPLastFavoriteIndex                    = @"LastFavoriteIndex";
 NSString *SPAutoConnectToDefault                 = @"AutoConnectToDefault";
 NSString *SPDefaultViewMode                      = @"DefaultViewMode";
 NSString *SPLastViewMode                         = @"LastViewMode";
-NSString *SPDefaultEncoding                      = @"DefaultEncoding";
+NSString *SPDefaultEncoding                      = @"DefaultEncodingTag";
 NSString *SPUseMonospacedFonts                   = @"UseMonospacedFonts";
 NSString *SPDisplayTableViewVerticalGridlines    = @"DisplayTableViewVerticalGridlines";
 NSString *SPCustomQueryMaxHistoryItems           = @"CustomQueryMaxHistoryItems";

Source/SPDatabaseData.m
@@ -29,6 +29,7 @@
 @interface SPDatabaseData (PrivateAPI)
 
 - (NSMutableArray *)_getDatabaseDataForQuery:(NSString *)query;
+NSInteger _sortMySQL4CharsetEntry(NSDictionary *itemOne, NSDictionary *itemTwo, void *context);
 
 @end
 
@@ -392,8 +393,21 @@ const SPDatabaseCharSets charsets[] =
 		
 		// Try to retrieve the available character set encodings from the database
 		// Check the information_schema.character_sets table is accessible
-		if (serverMajorVersion >= 5)
-			[characterSetEncodings addObjectsFromArray:[self _getDatabaseDataForQuery:@"SELECT * FROM `information_schema`.`character_sets` ORDER BY `character_set_name` ASC"]];	
+		if (serverMajorVersion >= 5) {
+			[characterSetEncodings addObjectsFromArray:[self _getDatabaseDataForQuery:@"SELECT * FROM `information_schema`.`character_sets` ORDER BY `character_set_name` ASC"]];
+		} else if (serverMajorVersion == 4 && serverMinorVersion >= 1) {
+			NSArray *supportedEncodings = [self _getDatabaseDataForQuery:@"SHOW CHARACTER SET"];
+			supportedEncodings = [supportedEncodings sortedArrayUsingFunction:_sortMySQL4CharsetEntry context:nil];
+			for (NSDictionary *anEncoding in supportedEncodings) {
+				NSDictionary *convertedEncoding = [NSDictionary dictionaryWithObjectsAndKeys:
+													[anEncoding objectForKey:@"Charset"], @"CHARACTER_SET_NAME",
+													[anEncoding objectForKey:@"Description"], @"DESCRIPTION",
+													[anEncoding objectForKey:@"Default collation"], @"DEFAULT_COLLATE_NAME",
+													[anEncoding objectForKey:@"Maxlen"], @"MAXLEN",
+													nil];
+				[characterSetEncodings addObject:convertedEncoding];
+			}
+		}
 
 		// If that failed, get the list of character set encodings from the hard-coded list
 		if (![characterSetEncodings count]) {
@@ -451,6 +465,14 @@ const SPDatabaseCharSets charsets[] =
 	}
 
 	return array;
+}
+
+/**
+ * Sorts a 4.1-style SHOW CHARACTER SET result by the Charset key.
+ */
+NSInteger _sortMySQL4CharsetEntry(NSDictionary *itemOne, NSDictionary *itemTwo, void *context)
+{
+	return [[itemOne objectForKey:@"Charset"] compare:[itemTwo objectForKey:@"Charset"]];
 }
 
 @end

commit 136a8e6db3c1f82cb8174c552c047b6fd3f92559

- Fix table creation to use the selected encoding instead of always using utf8 (broken in r2325)
git-svn-id: http://sequel-pro.googlecode.com/svn/trunk@2466 eefc5464-0a4a-0410-82e6-d75a05d1654a

Source/SPTablesList.m
@@ -2066,7 +2066,8 @@
 		
 	// If there is an encoding selected other than the default we must specify it in CREATE TABLE statement
 	if ([tableEncodingButton indexOfSelectedItem] > 0) {
-		charSetStatement = [NSString stringWithFormat:@"DEFAULT CHARACTER SET %@", [[tableDocumentInstance mysqlEncodingFromEncodingTag:[tableEncodingButton tag]] backtickQuotedString]];
+		NSString *encodingName = [[tableEncodingButton title] stringByMatching:@"\\((.*)\\)" capture:1L];
+		charSetStatement = [NSString stringWithFormat:@"DEFAULT CHARACTER SET %@", [[tableDocumentInstance mysqlEncodingFromEncodingTag:encodingName] backtickQuotedString]];
 	}
 	
 	// If there is a type selected other than the default we must specify it in CREATE TABLE statement